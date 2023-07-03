7ca8b4b6a12dedeed1df9d374ae421e3831cb166
Mr.Stijket <mr.stijket@gmail.com>
2023-07-03 23:58:24 +0300

    2/5

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,13 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+
+
+
 const register = async (req, res) => {
-  res.send('register user');
+  const user = await User.create({ ...req.body });
+  const token = jwt.sign({ userId: user_id, name: user, name }, 'jwtSecret', { expiresIn: '30d' })
+  res.status(StatusCodes.CREATED).json({ token });
 }
 
 const login = async (req, res) => {
--- routes/jobs.js
+++ routes/jobs.js
@@ -3,4 +3,7 @@ const router = express.Router();
 
 const { getAllJobs, getJob, createJob, updateJob, deleteJob } = require('../controllers/jobs');
 
-router.route('/').post()
+router.route('/').post(createJob).get(getAllJobs);
+router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob);
+
+module.exports = router;
