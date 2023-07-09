c59f0487f3e756b8989020aa637b4ee0dc80db56
Mr.Stijket <mr.stijket@gmail.com>
2023-07-09 11:32:57 +0300

    Before Heroku / Final

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
-// connectDB
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const connectDB = require('./db/connect');
+const authenticatedUser = require('./middleware/authentication');
 
 // routers
 const authRouter = require('./routes/auth');
@@ -14,12 +20,19 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+}));
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobsRouter);
+app.use('/api/v1/jobs', authenticatedUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
